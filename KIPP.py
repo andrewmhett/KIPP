61628dd43506d8107333b4b04081dd2f7d99c42a
LockdownDoom <andrewmhett@gmail.com>
2019-07-23 13:46:57 -0700

    Automated Backup

--- KIPP.py
+++ KIPP.py
@@ -10,11 +10,7 @@
 import sys
 import logging
 import os
-#print(sys.path)
-#try:
-#        sys.path.append('./KIPPSTUFF')
-#except:
-#	pass
+sys.path.append('./KIPPSTUFF')
 from ESSENTIAL_PACKAGES import *
 CREATOR_ID="289920025077219328"
 KIPP_ID="386352783550447628"

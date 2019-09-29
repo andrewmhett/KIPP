af8739f62836840ee22ad720906d40fc26bf5e57
LockdownDoom <andrewmhett@gmail.com>
2019-09-28 22:41:52 -0700

    Automated Backup

--- KIPP_MONITOR.py
+++ KIPP_MONITOR.py
@@ -18,26 +18,19 @@ KIPP_RESET_ERRORS = [ConnectionResetError,
                      websockets.exceptions.InvalidStatusCode]
 try:
     logging.log(50,"Backing KIPP up to GitHub...")
-    #p=Popen(os.system('sudo /home/pi/Desktop/KIPPSTUFF/BackupKIPP.sh'),stdout=PIPE,stderr=PIPE)
-    #p.communicate()[0]
-    #p.kill()
     os.system('sudo /home/pi/Desktop/KIPPSTUFF/BackupKIPP.sh')
 except FileNotFoundError:
     logging.log(50,"KIPP backup file not found.")
 try:
     logging.log(50,"Checking for package updates")
-    p=Popen(os.system('sudo /home/pi/Desktop/KIPPSTUFF/REQUIREMENTUPDATES.sh'),stdout=PIPE,stderr=PIPE)
-    p.communicate()
-    p.kill()
+    os.system('sudo /home/pi/Desktop/KIPPSTUFF/REQUIREMENTUPDATES.sh')
 except FileNotFoundError:
     logging.log(50,"KIPP package updater not found.")
 while True:
     logging.log(50,"KIPP starting...")
     try:
-        p=Popen(os.system("sudo python3.6 /home/pi/Desktop/KIPP.py"),stdout=PIPE,stderr=PIPE)
-        stdout=p.communicate()[0]
+        os.system("sudo python3.6 /home/pi/Desktop/KIPP.py")
     except Exception as e:
         if type(e) in KIPP_RESET_ERRORS:
             pass
-    p.kill()
     logging.log(50,"KIPP restarting...")

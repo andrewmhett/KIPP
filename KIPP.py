d11c282c705f5584075d33b77f9ff1d011282240
LockdownDoom <andrewmhett@gmail.com>
2020-01-31 22:00:58 -0800

    Automated Backup

--- KIPP.py
+++ KIPP.py
@@ -1422,7 +1422,7 @@ async def APPENDPLAYLIST(message,message2):
                     serverinfo[message.server].loading = False
                     return
             serverinfo[message.server].loading = False
-            arr=serverinfo[message.server].search_server_configs("PLAYLIST:{0}".format(name))[1]
+            arr=serverinfo[message.server].search_server_configs("PLAYLIST:{0}".format(name))[0][1]
             youtube = etree.HTML(urllib.request.urlopen(music4).read())
             song=youtube.xpath("//span[@id='eow-title']/@title")[0]
             arr.append([song,music4])

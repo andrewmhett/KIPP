7cf1228e7e2ff0c6769250e0e783963886bd33a5
LockdownDoom <andrewmhett@gmail.com>
2020-02-01 00:41:24 -0800

    Automated Backup

--- KIPP.py
+++ KIPP.py
@@ -84,7 +84,7 @@ class Server:
             arr=[]
         arr.append(data)
         with open('/home/pi/Desktop/KIPPSTUFF/ServerConfigs/{0}'.format(self.server.id),'w') as f:
-            writer=csv.writer(f, delimiter="~,")
+            writer=csv.writer(f, delimiter='~,')
             for row in arr:
                 writer.writerow(row)
             f.close()
@@ -99,7 +99,7 @@ class Server:
                 break
             cntr=cntr+1
         with open('/home/pi/Desktop/KIPPSTUFF/ServerConfigs/{0}'.format(self.server.id),'w') as f:
-            writer=csv.writer(f,delimiter="~,")
+            writer=csv.writer(f,delimiter='~,')
             for row in arr:
                 writer.writerow(row)
             f.close()

16d7f254e4615ce449d98c0860428cf810185f3a
LockdownDoom <andrewmhett@gmail.com>
2020-05-16 20:55:55 -0700

    Automated Backup

--- Python/KIPP.py
+++ Python/KIPP.py
@@ -18,6 +18,7 @@ from Server import Server
 from Music import search_music, music_handler, YTDLSource
 from Profile import Profile
 from Commands import *
+from FooterGenerator import get_footer
 from config import *
 MSG_COUNTER=0
 client=discord.Client()
@@ -132,104 +133,26 @@ while True:
                print("Welcome channel was deleted, couldn't send message to welcome channel")
     @client.event
     async def on_message(message):
-        global MSG_COUNTER
-        MSG_COUNTER=MSG_COUNTER+1
-        if message.guild != None or str(message.channel).upper=="DIRECT MESSAGE":
-            if message.author.id in serverinfo[message.guild].blocked:
-                await client.delete_message(message)
-                return
-        if str(message.channel).upper().startswith('DIRECT MESSAGE') == False:
-            serverinfo[message.guild].recentchannel = message.channel
-        if str(message.guild) != "None":
-            user = message.guild.get_member(KIPP_ID)
-            playerinfo[message.author].game = str(message.author.activity)
-        else:
+        if message.author == client.user or message.guild==None:
             return
+        if message.author.id in serverinfo[message.guild].blocked:
+            await client.delete_message(message)
+            return
+        serverinfo[message.guild].recentchannel = message.channel
         kippservers = 0
         roles = []
         for server in client.guilds:
             for member in server.members:
                 if member == message.author:
                     kippservers=kippservers+1
-        if str(message.guild) != "None":
-            playerinfo[message.author].nickname = str(message.author.nick)
-            playerinfo[message.author].highestrole = message.author.top_role
-            role1 = discord.utils.get(message.author.guild.roles, name=str(playerinfo[message.author].highestrole))
-            playerinfo[message.author].hrolecolor = role1.colour
-            playerinfo[message.author].numkippservers = kippservers
-        weekint = datetime.today().weekday()
-        if weekint == 0:
-            weekday = "Monday"
-        if weekint == 1:
-            weekday = "Tuesday"
-        if weekint == 2:
-            weekday = "Wednesday"
-        if weekint == 3:
-            weekday = "Thursday"
-        if weekint == 4:
-            weekday = "Friday"
-        if weekint == 5:
-            weekday = "Saturday"
-        if weekint == 6:
-            weekday = "Sunday"
-        year = ((str(datetime.now())[0])+(str(datetime.now())[1])+(str(datetime.now())[2])+(str(datetime.now())[3]))
-        month = ((str(datetime.now())[5])+(str(datetime.now())[6]))
-        day = ((str(datetime.now())[8])+(str(datetime.now())[9]))
-        minute = ((str(datetime.now())[14])+(str(datetime.now())[15]))
-        if (int((str(datetime.now())[11])+(str(datetime.now())[12]))>12) or (int((str(datetime.now())[11])+(str(datetime.now())[12])) == 12):
-            timeBinary = 1
-            if int((str(datetime.now())[11])+(str(datetime.now())[12]))>12:
-                hour = str(int((str(datetime.now())[11])+(str(datetime.now())[12]))-12)
-            else:
-                hour = str(int((str(datetime.now())[11])+(str(datetime.now())[12])))
-        else:
-            hour = ((str(datetime.now())[11])+(str(datetime.now())[12]))
-            timeBinary = 0
-        if str(hour) == "00":
-            hour = "12"
-        if str(month) == "01":
-            strmonth = "January"
-        if str(month) == "02":
-            strmonth = "February"
-        if str(month) == "03":
-            strmonth = "March"
-        if str(month) == "04":
-            strmonth = "April"
-        if str(month) == "05":
-            strmonth = "May"
-        if str(month) == "06":
-            strmonth = "June"
-        if str(month) == "07":
-            strmonth = "July"
-        if str(month) == "08":
-            strmonth = "August"
-        if str(month) == "09":
-            strmonth = "September"
-        if str(month) == "10":
-            strmonth = "October"
-        if str(month) == "11":
-            strmonth = "November"
-        if str(month) == "12":
-            strmonth = "December"
-        if day == "11" or day == "12" or str(day) == "13" or (str(day).endswith('1') == False and day != "11") or (str(day).endswith('2') == False and str(day) != "12") or (str(day).endswith('3') == False and day != "13"):
-            ending = "th"
-        if str(day).endswith("1") and str(day) != "11":
-            ending = "st"
-        if str(day).endswith("2") and str(day) != "12":
-            ending = "nd"
-        if str(day).endswith("3") and str(day) != "13":
-            ending = "rd"
-        if str(day).startswith('0'):
-            day = str(day)
-            day = day.split('0')[1]
-        global profooter
-        if timeBinary == 0:
-            profooter = ("KIPP | "+weekday+" "+strmonth+" "+str(day)+ending+", "+str(year)+" at "+hour+":"+minute+" AM")
-        if timeBinary == 1:
-            profooter = ("KIPP | "+weekday+" "+strmonth+" "+str(day)+ending+", "+str(year)+" at "+hour+":"+minute+" PM")
+        playerinfo[message.author].nickname = str(message.author.nick)
+        playerinfo[message.author].highestrole = message.author.top_role
+        role1 = discord.utils.get(message.author.guild.roles, name=str(playerinfo[message.author].highestrole))
+        playerinfo[message.author].hrolecolor = role1.colour
+        playerinfo[message.author].numkippservers = kippservers
+        playerinfo[message.author].game = message.author.activity
+        profooter=get_footer()
         message2 = str(message.content).upper()
-        if message.author == client.user:
-            return
         if message.guild.get_member(KIPP_ID).mention in message2:
             await message.channel.send("What do you want? Use **!help** for the commands.")
         if message2.startswith("!HELP|"):

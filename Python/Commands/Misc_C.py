64732e12b2fb96e740ad9f5a3a3c5aaa820e091d
LockdownDoom <andrewmhett@gmail.com>
2020-05-18 17:52:58 -0700

    Automated Backup
    Signed-off-by: LockdownDoom <andrewmhett@gmail.com>

--- Python/Commands/Misc_C.py
+++ Python/Commands/Misc_C.py
@@ -15,6 +15,7 @@ async def CODE(message,message2,serverinfo,playerinfo):
     except discord.DiscordException:
         commit_msg = 'The newest commit was to large to be displayed here.\n'
     emb.discription="{0} My code is backed up on GitHub [here](https://github.com/LockdownDoom/KIPP/blob/master/KIPP.py)\nAlso, my code has been reviewed by Codacy [here](https://app.codacy.com/project/LockdownDoom/KIPP/dashboard?branchId=10423847)".format(commit_msg)
+    await message.channel.send(embed=emb)
 
 async def GRAPH(message,message2,serverinfo,playerinfo):
     img = Image.new('RGB', (1000,1000), "black")

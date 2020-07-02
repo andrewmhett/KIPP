f00072821682df7f407ea40037070e61d520806c
LockdownDoom <andrewmhett@gmail.com>
2020-07-02 00:41:39 -0700

    Directly upload facedetect image

--- Python/Commands/Misc_C.py
+++ Python/Commands/Misc_C.py
@@ -5,10 +5,8 @@ from Command import *
 async def FACEDETECT(message,message2,serverinfo,playerinfo):
     os.system("sudo curl \"{0}\" -o img.jpg".format(str(message.content).split("|")[1]))
     os.system("sudo /home/pi/openvino/deployment_tools/inference_engine/samples/build/armv7l/Release/object_detection_sample_ssd -m /home/pi/openvino/deployment_tools/inference_engine/samples/build/face-detection-adas-0001.xml -d MYRIAD -i img.jpg")
-    embed = discord.Embed(title="!FaceDetect", color=EMBEDCOLOR) #creates embed
-    file = discord.File("out_0.bmp",filename="image.png")
-    embed.set_image(url="attachment://image.png")
-    await message.channel.send(file=file,embed=embed)
+    file = discord.File("out_0.bmp")
+    await message.channel.send(file=file)
 
 async def CODE(message,message2,serverinfo,playerinfo):
     from subprocess import Popen, PIPE

6f8e6fa3fa138df7734af9edb76f16bd43193f4c
LockdownDoom <andrewmhett@gmail.com>
2020-08-29 13:01:45 -0700

    Update Misc_C.py

--- Python/Commands/Misc_C.py
+++ Python/Commands/Misc_C.py
@@ -1,6 +1,8 @@
 from ESSENTIAL_PACKAGES import *
 from .Command_utils import *
 from Command import *
+import multiprocessing
+import ctypes
 
 async def FACEDETECT(message,message2,serverinfo,playerinfo):
     import subprocess
@@ -63,8 +65,7 @@ async def CORETEMP(message,message2,serverinfo,playerinfo):
     tmp.close()
     await message.channel.send( '{:.2f}'.format( float(cpu)/1000 ) + ' C')
 
-async def IMAGE(message,message2,serverinfo,playerinfo):
-    await message.channel.send( "Processing image request...")
+def locate_image(message2,image):
     url = "https://www.google.com/search?tbm=isch&source=hp&biw=2560&bih=1309&ei=eCYOW5bML6Oi0gK774NY&q={0}&oq={1}&gs_l=img.3..0l10.3693.4072.0.4294.7.6.0.1.1.0.59.152.3.3.0....0...1ac.1.64.img..3.4.156.0...0.OLvQBmMFRWY".format(message2.split('|')[1].replace(' ','+').replace("'","%27"),message2.split('|')[1].replace(' ','+').replace("'","%27"))
     headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     req = requests.get(url, headers=headers)
@@ -75,15 +76,25 @@ async def IMAGE(message,message2,serverinfo,playerinfo):
         try:
             i=i+1
             if cont>300:
-                await message.channel.send("No results for image search **{0}**".format(str(message.content).split('|')[1]))
+                image.value=""
                 break
             if ".JPEG" in ("https://"+str(req).split('https://')[i].split('"')[0]).upper() or ".JPG" in ("https://"+str(req).split('https://')[i].split('"')[0]).upper():
-                image = "https://"+str(req).split('https://')[i].split('"')[0]
-                if requests.get(image).status_code==200:
-                    await send_image(message,image,"Image")
+                image.value = "https://"+str(req).split('https://')[i].split('"')[0]
+                if requests.get(image.value).status_code==200:
                     break
         except Exception:
-            cont=cont+1
+            cont+=1
+
+async def IMAGE(message,message2,serverinfo,playerinfo):
+    await message.channel.send("Processing image request...")
+    image=multiprocessing.Value(ctypes.c_wchar_p,"")
+    proc=multiprocessing.Process(target=locate_image,args=(message2,image))
+    proc.start()
+    proc.join()
+    if image.value == "":
+        await message.channel.send("No results for image search **{0}**".format(str(message.content).split('|')[1]))
+    else:
+        await send_image(message,image.value,"Image")
 
 async def GIF(message,message2,serverinfo,playerinfo):
     await message.channel.send( "Processing gif request...")

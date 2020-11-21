e74750ace2000dc5b0bdd14635ebcaf91856ecd1
Andrew Hett <andrewmhett@gmail.com>
2020-11-21 13:55:19 -0800

    Modified KIPPCOINS_IO to use char* instead of long long for IDs

--- C++/KIPPCOINS_IO.cpp
+++ C++/KIPPCOINS_IO.cpp
@@ -3,14 +3,19 @@
 #include <map>
 #include <vector>
 #include <stdlib.h>
+#include <cstring>
 
 using namespace std;
 
-map<long long, unsigned int> accounts;
+map<string, unsigned int> accounts;
 
 #pragma pack(push, 1)
 struct Account{
-	long long id;
+	string id;
+	unsigned int balance;
+};
+struct StoredAccount{
+	char id[19];
 	unsigned int balance;
 };
 #pragma pack(pop)
@@ -24,9 +29,13 @@ void map_data(){
 	char char_buf;
 	while (input >> char_buf){
 		input.putback(char_buf);
-		Account read_struct;
-		input.read(reinterpret_cast<char*>(&read_struct),sizeof(Account));
-		accounts_v.push_back(read_struct);
+		StoredAccount read_struct;
+		input.read(reinterpret_cast<char*>(&read_struct),sizeof(StoredAccount));
+		Account new_struct;
+		string id_str(read_struct.id);
+		new_struct.id=id_str;
+		new_struct.balance=read_struct.balance;
+		accounts_v.push_back(new_struct);
 	}
 	input.close();
 	for(vector<Account>::iterator it = accounts_v.begin(); it != accounts_v.end(); ++it){
@@ -39,23 +48,23 @@ void write_data(){
 	KIPP_PATH=getenv("KIPP_DIR");
 	ofstream output;
 	output.open(KIPP_PATH+"/C++/KIPPCOINS.bin", ios::binary);
-	for (map<long long, unsigned int>::iterator it = accounts.begin(); it != accounts.end(); ++it){
-		Account write_struct;
-		pair<long long, unsigned int> account = *it;
-		write_struct.id=account.first;
+	for (map<string, unsigned int>::iterator it = accounts.begin(); it != accounts.end(); ++it){
+		StoredAccount write_struct;
+		pair<string, unsigned int> account = *it;
+		strcpy(write_struct.id,account.first.c_str());
 		write_struct.balance = account.second;
-		output.write(reinterpret_cast<char*>(&write_struct),sizeof(Account));
+		output.write(reinterpret_cast<char*>(&write_struct),sizeof(StoredAccount));
 	}
 	output.close();
 }
-void edit_balance(long long id, int balance){
+void edit_balance(string id, int balance){
 	accounts[id] = balance;
 	cout << id << ": " << balance << endl;
 }
-void read_balance(long long id){
-	if (id==0){
-		for (map<long long, unsigned int>::iterator it = accounts.begin(); it != accounts.end(); ++it){
-			pair<long long, unsigned int> account = *it;
+void read_balance(string id){
+	if (id=="all"){
+		for (map<string, unsigned int>::iterator it = accounts.begin(); it != accounts.end(); ++it){
+			pair<string, unsigned int> account = *it;
 			cout << account.first << ": " << flush;
 			cout << account.second << endl;
 		}
@@ -67,13 +76,13 @@ int main(int argc, const char * argv[]){
 	map_data();
 	if (*argv[1] == 'r'){
 		if (*argv[2] == 'a'){
-			read_balance(0);
+			read_balance("all");
 		}else{
-			read_balance(atoll(argv[2]));
+			read_balance(argv[2]);
 		}
 	}
 	if (*argv[1] == 'w'){
-		edit_balance(atoll(argv[2]), stoul(argv[3]));
+		edit_balance(argv[2], stoul(argv[3]));
 	}
 	write_data();
 	return 0;

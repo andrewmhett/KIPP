bc79de382e333922bd9fb08e61d42aecd4811887
Andrew Hett <andrewmhett@gmail.com>
2020-12-10 11:29:39 -0800

    Update test_KIPP.py

--- Python/test_KIPP.py
+++ Python/test_KIPP.py
@@ -1,10 +1,10 @@
+import os
+try:
+    KIPP_DIR = os.environ['KIPP_DIR']
+except KeyError:
+    os.environ['KIPP_DIR']=os.environ['PWD']
+    KIPP_DIR=os.environ['PWD']
+from ESSENTIAL_PACKAGES import *
 def test_requirements():
-    import os
-    try:
-        KIPP_DIR = os.environ['KIPP_DIR']
-    except KeyError:
-        os.environ['KIPP_DIR']=os.environ['PWD']
-        KIPP_DIR=os.environ['PWD']
-    from ESSENTIAL_PACKAGES import *
     import Commands
     from Command import commands

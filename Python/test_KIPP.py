e1963d685af715b9b60b6fa7f9f055b8aa832b2e
Andrew Hett <andrewmhett@gmail.com>
2020-12-10 11:24:27 -0800

    Update test_KIPP.py

--- Python/test_KIPP.py
+++ Python/test_KIPP.py
@@ -7,7 +7,3 @@ except KeyError:
 from ESSENTIAL_PACKAGES import *
 import Commands
 from Command import commands
-
-async def test_kipp():
-  for command in commands:
-    await command.Exec(discord.Message,command.Name,{},{})
